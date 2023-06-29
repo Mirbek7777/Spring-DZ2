17e778ffd56dccfc771e23c415c7ed6c9e1af962
Mirbek <mirbek_karimberdiev@mail.ru>
2023-06-29 23:19:05 +0300

    1th commit

---
+++ src/main/java/netology/ru/Handler.java
@@ -0,0 +1,9 @@
+package netology.ru;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface Handler {
+    public void handle(Request request, BufferedOutputStream responseStream) throws IOException;
+}
